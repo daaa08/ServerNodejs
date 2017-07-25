02c1dbfb10020301ae16a8567c792cfd9881771e
박다정 <daaa08@naver.com>
2017-07-26 00:17:34 +0900

    입력값이 null일때 data가 입력되지 않도록

--- app/src/main/java/com/example/da08/servernodejs/WriteActivity.java
+++ app/src/main/java/com/example/da08/servernodejs/WriteActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -49,27 +50,36 @@ public class WriteActivity extends AppCompatActivity {
         bbs.author = author;
         bbs.content = content;
 
-        // 1 레트로핏 생성
-        Retrofit client = new Retrofit.Builder()
-                .baseUrl(Ibbs.SERVER)
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
-        // 2 서비스 연결
-        Ibbs myServer = client.create(Ibbs.class);
-        // 3 서비스의 특정 함수 호출 -> Observable 생성
-        Gson gson = new Gson();
-        // bbs 객체를 수동으로 전달하기 위해서는 bbs객체를 수동으로 변환하고 requestBody에 미디어타입과 String으로 변환된 데이터를 담아서 전송
-        RequestBody body = RequestBody.create(
-                MediaType.parse("application/json"),
-                gson.toJson(bbs)
-        );
-        Observable<ResponseBody> observable = myServer.write(body);
-        // 4 subscribe 등록
-        observable.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        responseBody -> {
-                            String result = responseBody.string();  // 결과코드를 넘겨서 처리
+        // 입력값이 null일때
+        if(bbs.title == null || bbs.title.equals("") || bbs.title.equals(null)
+                || bbs.author == null || bbs.author.equals("") || bbs.author.equals(null)
+                || bbs.content == null || bbs.content.equals("") || bbs.content.equals(null)){
+
+            Toast.makeText(getBaseContext(),"빈 칸을 채워주세요",Toast.LENGTH_SHORT).show();
+
+        }else {  // 입력값이 모두 입력되었을때
+
+            // 1 레트로핏 생성
+            Retrofit client = new Retrofit.Builder()
+                    .baseUrl(Ibbs.SERVER)
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+            // 2 서비스 연결
+            Ibbs myServer = client.create(Ibbs.class);
+            // 3 서비스의 특정 함수 호출 -> Observable 생성
+            Gson gson = new Gson();
+            // bbs 객체를 수동으로 전달하기 위해서는 bbs객체를 수동으로 변환하고 requestBody에 미디어타입과 String으로 변환된 데이터를 담아서 전송
+            RequestBody body = RequestBody.create(
+                    MediaType.parse("application/json"),
+                    gson.toJson(bbs)
+            );
+            Observable<ResponseBody> observable = myServer.write(body);
+            // 4 subscribe 등록
+            observable.subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            responseBody -> {
+                                String result = responseBody.string();  // 결과코드를 넘겨서 처리
 
                             /*
                             1. 정상적으로 새 글을 입력한 후 데이터를 전송하고 종료
@@ -77,10 +87,11 @@ public class WriteActivity extends AppCompatActivity {
 
                             위의 두 가지를 구분해서 결과값을 호출한 MainActivity로 넘겨서 처리
                              */
-                            finish();
+                                finish();
 
-                        }
-                );
+                            }
+                    );
+        }
 
     }
 
