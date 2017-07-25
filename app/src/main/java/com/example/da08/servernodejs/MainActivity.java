ddb116af9ef4224376a70d3692278026b67f911f
박다정 <daaa08@naver.com>
2017-07-25 11:09:57 +0900

    lambda test

--- app/src/main/java/com/example/da08/servernodejs/MainActivity.java
+++ app/src/main/java/com/example/da08/servernodejs/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.da08.servernodejs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lambdaTest();
     }
-    
+    private void lambdaTest(){
+        new Thread(
+                ()-> Log.i("Labmda","-===========================-")
+        ).start();
+    }
 }
